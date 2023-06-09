3879d591935e368a11cd2a280a3220a1412a38b9
ytakahashi2020 <y.takahashi2020@gmail.com>
2023-06-09 14:37:44 +0900

    mappingの追加

--- contracts/psp34/lib.rs
+++ contracts/psp34/lib.rs
@@ -9,6 +9,8 @@ pub mod my_psp34_mintable {
     };
     use openbrush::traits::String;
 
+    use openbrush::storage::Mapping;
+
     #[derive(Default, Storage)]
     #[ink(storage)]
     pub struct Contract {
@@ -19,6 +21,27 @@ pub mod my_psp34_mintable {
         pub normal_uri: String,
         pub good_uri: String,
         pub bad_uri: String,
+
+        // for random
+        pub salt: u64,
+
+        // last eaten time
+        pub last_eaten: Mapping<Id, u64>,
+
+        // last daily bonus time
+        pub last_bonus: Mapping<AccountId, u64>,
+
+        // last staked time
+        pub last_staked: Mapping<AccountId, u64>,
+
+        // apple number the account has
+        pub apple_number: Mapping<AccountId, u16>,
+
+        // game money the account has
+        pub your_money: Mapping<AccountId, u64>,
+
+        // staked game noney the account has
+        pub your_staked_money: Mapping<AccountId, u64>,
     }
 
     impl PSP34 for Contract {}
